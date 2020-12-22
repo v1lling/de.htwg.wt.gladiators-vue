f38d8af50e9619d95c3d706ecc348f98fb8ae639
Sascha Villing <sascha.villing@web.de>
2020-12-22 15:09:01 +0100

    Set up Router & Store

--- src/main.js
+++ src/main.js
@@ -2,9 +2,14 @@ import Vue from 'vue'
 import App from './App.vue'
 import vuetify from './plugins/vuetify';
 
+import store from "./store.js";
+import router from './router/index.js'
+
 Vue.config.productionTip = false
 
 new Vue({
   vuetify,
+  store: store,
+  router,
   render: h => h(App)
 }).$mount('#app')
