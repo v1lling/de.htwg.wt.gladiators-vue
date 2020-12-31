806591d1df3fd2550197ef2f8218b47bb90f128c
Sascha Villing <sascha.villing@web.de>
2020-12-31 14:22:49 +0100

    Update vuetify navbar

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,6 @@
 <template>
   <div id = "app">
     <v-toolbar >
-      <v-app-bar-nav-icon v-if="window.width<400" @click.stop="drawer = !drawer" class="hidden-md-and-up"></v-app-bar-nav-icon>
       <v-img
         src="@/assets/logo.png"
         max-height="40"
@@ -10,6 +9,7 @@
       </v-img>
       <v-toolbar-title class="v-title">Gladiators</v-toolbar-title>
       <v-spacer></v-spacer>
+      <v-app-bar-nav-icon v-if="window.width<400" @click.stop="drawer = !drawer" class="hidden-md-and-up"></v-app-bar-nav-icon>
       <v-toolbar-items v-if="window.width>=400" class="hidden-sm-and-down">
         <v-btn text to="/">Game</v-btn>
         <v-btn text to="/About">Rules</v-btn>
@@ -19,12 +19,24 @@
     <v-navigation-drawer
         v-model="drawer"
         temporary
+        right
         absolute
         width = "200"
         id = "drawer">
-        <v-btn text to="/">Game</v-btn>
-        <v-btn text to="/About">About</v-btn>
-
+        <v-list
+          nav
+          dense>
+          <v-list-item-group
+            v-model="group"
+            active-class="deep-purple--text text--accent-4">
+            <v-list-item to="/">
+              <v-list-item-title >Game</v-list-item-title>
+            </v-list-item>
+            <v-list-item to="/About">
+              <v-list-item-title >Rules</v-list-item-title>
+            </v-list-item>
+          </v-list-item-group>
+        </v-list>
     </v-navigation-drawer>
     <router-view/>
   </div>
