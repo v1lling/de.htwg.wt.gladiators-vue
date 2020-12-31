44a602701b28ff0ea53da6e61bbcd22c98c5b8f9
Sascha Villing <sascha.villing@web.de>
2020-12-31 14:32:44 +0100

    Add background to body

--- src/App.vue
+++ src/App.vue
@@ -38,7 +38,7 @@
           </v-list-item-group>
         </v-list>
     </v-navigation-drawer>
-    <router-view/>
+    <router-view class="v-router"/>
   </div>
 </template>
 
@@ -70,6 +70,11 @@
 </script>
 
 <style lang="scss">
+body {
+  background: #2980B9;  /* fallback for old browsers */
+  background: -webkit-linear-gradient(to top, #FFFFFF, #6DD5FA, #2980B9);  /* Chrome 10-25, Safari 5.1-6 */
+  background: linear-gradient(to top, #FFFFFF, #6DD5FA, #2980B9); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -81,13 +86,12 @@
   padding-left: 1rem;
 }
 .swipe-enter-active,
-    .swipe-leave-active {
-        transition: opacity 0.7s, transform 0.7s;
-    }
-
-    .swipe-enter,
-    .swipe-leave-active {
-        opacity: 0;
-        transform: scale(0);
-    }
+.swipe-leave-active {
+    transition: opacity 0.7s, transform 0.7s;
+}
+.swipe-enter,
+.swipe-leave-active {
+    opacity: 0;
+    transform: scale(0);
+}
 </style>
