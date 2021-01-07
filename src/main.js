36506acf9f3517cedb51b34e1113f198fc4f4396
Sascha Villing <sascha.villing@web.de>
2021-01-07 13:13:51 +0100

    Add missing semicolons

--- src/main.js
+++ src/main.js
@@ -1,12 +1,12 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import App from './App.vue';
 import Vuetify from "vuetify";
 
 import store from "./store.js";
-import router from './router/index.js'
+import router from './router/index.js';
 import vuetify from './plugins/vuetify';
-import vmodal from 'vue-js-modal'
-import './registerServiceWorker'
+import vmodal from 'vue-js-modal';
+import './registerServiceWorker';
 Vue.config.productionTip = false
 Vue.use(Vuetify);
 Vue.use(vmodal);
