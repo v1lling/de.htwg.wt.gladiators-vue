fbf9572ef2fb3b3dc747ce8fe89be3490780f5ee
Sascha Villing <sascha.villing@web.de>
2021-01-17 13:10:46 +0100

    Update user page

--- src/views/User.vue
+++ src/views/User.vue
@@ -1,9 +1,9 @@
 <template>
     <div class="loginpage pt-8">
-        <h2>User</h2>
-        First name: {{user.firstName}}
-        Last name: {{user.lastName}}
-        E-mail: {{user.email}}
+        <h1>User</h1>
+        <h2>First name: {{user.firstName}}</h2>
+        <h2>Last name: {{user.lastName}}</h2>
+        <h2>E-mail: {{user.email}}</h2>
         <div class="form-group pt-4">
             <v-btn @click=logout>Logout</v-btn>
         </div>
