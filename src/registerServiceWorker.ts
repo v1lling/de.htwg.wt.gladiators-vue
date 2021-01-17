9e414b16af73c09c705c9035c1ce3cb708a387d2
Sascha Villing <sascha.villing@web.de>
2021-01-17 09:54:50 +0100

    Remove production condition from service worker

--- src/registerServiceWorker.ts
+++ src/registerServiceWorker.ts
@@ -3,7 +3,7 @@
 import { register } from 'register-service-worker'
 import store from './store';
 
-if (process.env.NODE_ENV === 'production') {
+//if (process.env.NODE_ENV === 'production') {
   register(`${process.env.BASE_URL}service-worker.js`, {
     ready () {
       console.log(
@@ -32,4 +32,4 @@ if (process.env.NODE_ENV === 'production') {
       console.error('Error during service worker registration:', error)
     }
   })
-}
+//}
